ff1044e165c22bf5c82b1eb60acbfcd41df1237b
Lior Hasson <liorhasson@gmail.com>
2019-12-17 20:44:48 +0200

    A few changes after Nir gave his comments

---
+++ script.js
@@ -0,0 +1,13 @@
+function yellowImg(my_image) {
+
+    my_image.src = "images/read_more_yellow.png";
+
+
+}
+
+
+function normalImg(my_image) {
+
+    my_image.src = "images/read_more_white.png";
+
+}
